9eedac5121ad36c053227102cbd63aa9734373d6
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-08-19 16:06:57 +0300

    rectangle class

--- rectangle.java
+++ rectangle.java
@@ -19,28 +19,60 @@ kannat, korkeudet ja pinta-alat.
 public class main {
     public static void main(String[] args) {
         rectangle test;
+        double low[] = {0,0};
+        double high[] = {10,10};
+        test = new rectangle(low, high);
+        test.get_data();
         test = new rectangle();
         test.get_data();
     }
 
 }
 public class rectangle {
+    private static final int SIDE = 1;  
+    private static final int BASE = 0;  
     double lower_left_corner[];
     double high_right_corner[];
+    double base_lenght;
+    double side_height;
+    double area;
     public rectangle() {
         lower_left_corner = new double[2];
         high_right_corner = new double[2];
-        lower_left_corner[0] = 0;
-        lower_left_corner[1] = 0;
-        high_right_corner[0] = 1;
-        high_right_corner[1] = 1;
-        
+        lower_left_corner[BASE] = 0;
+        lower_left_corner[SIDE] = 0;
+        high_right_corner[BASE] = 1;
+        high_right_corner[SIDE] = 1;
+        base_lenght();
+        side_lenght();
+        set_area();
+    }
+    public rectangle(double lower_corner[], double higher_corner[]) {
+        lower_left_corner = new double[2];
+        high_right_corner = new double[2];
+        lower_left_corner[BASE] = lower_corner[BASE];
+        lower_left_corner[SIDE] = lower_corner[SIDE];
+        high_right_corner[BASE] = higher_corner[BASE];
+        high_right_corner[SIDE] = higher_corner[SIDE];;
+        base_lenght();
+        side_lenght();
+        set_area();
     }
     public void get_data() {
         System.out.println("lower left xy: " + lower_left_corner[0] + " " + lower_left_corner[1]);
         System.out.println("higher right xy: " + high_right_corner[0] + " " + high_right_corner[1]);
+        System.out.println("Base lenght: " + base_lenght);
+        System.out.println("Side height: " + side_height);
+        System.out.println("Area is: " + area);
+
     }
     public void base_lenght() {
-
+        base_lenght = high_right_corner[BASE] - lower_left_corner[BASE];
+    }
+    public void side_lenght() {
+        side_height = high_right_corner[SIDE] - lower_left_corner[SIDE];
+    }
+    public void set_area() {
+        area = base_lenght * side_height;
     }
 }
