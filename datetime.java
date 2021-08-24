f5e749fa42cb94ad24f0df686fa8012632cbb275
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-08-24 18:59:22 +0300

    exercise done

--- datetime.java
+++ datetime.java
@@ -1,62 +1,24 @@
-// Tehtävä 2
-// a) Kirjoita luokka, joka kuvaa päivämäärän piirteet (tietoina kokonaisluvut päivä, kuukausi ja vuosi).
-// Määrittele tiedot yksityisiksi ja kirjoita niille setterit ja getterit (Vinkki: saat setterit ja getterit Eclipsen
-// avulla valitsemalla Source-valikosta Generate Getters and Setters…). Kirjoita luokkaan myös metodi
-// public String toString()
-// joka palauttaa päivämäärän tiedot merkkijonona muodossa 8.2.2018.
-// b) Kirjoita parametriton konstruktori, joka alustaa päivämäärän järjestelmän päivämäärällä. Järjestelmän
-// päivämäärä saadaan java.time.LocalDate-luokan avulla:
-// java.time.LocalDate tanaan = java.time.LocalDate.now();
-// paiva = tanaan.getDayOfMonth();
-// kuukausi = tanaan.getMonthValue();
-// vuosi = tanaan.getYear();
-// c) Kirjoita konstruktori, jolle välitetään päivä, kuukausi ja vuosi kokonaislukuparametreina.
-// d) Kirjoita konstruktori, jolle välitetään päivämäärä merkkijonona ”pp.kk.vvvv”, mistä konstruktori
-// selvittää päivän, kuukauden ja vuoden. (Vinkki: katkaise tiedot merkkijonosta esim. split- tai substringmetodilla ja saatu merkkijono muutetaan int-tyyppiseksi Integer.parseInt(merkkijono)).
-// e) Kirjoita metodi public boolean onSama(Paivamaara toinen), joka palauttaa totuusarvon, onko
-// tämän olion päivämäärä sama kuin parametrina välitetty toinen päivämäärä. (Käytä parametrille
-// tyyppinä omaa kirjoittamaasi luokkatyyppiä Paivamaaran sijaan.)
-// Kirjoita main-metodi, jolla kokeilet konstruktoreiden ja vertailun toimintaa. Kysy ainakin jokin päivämäärä
-// käyttäjältä (esim. muodossa pp.kk.vvvv).
-// Jatka vielä päivämäärän käsittelyyn tarkoitettua luokkaa. Määrittele luokan olioille yhteinen Stringtaulukko, johon alustat kuukausien nimet:
-// public static final String[] KUUKAUDET = {"tammikuu", "helmikuu",
-// "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu",
-// "syyskuu", "lokakuu", "marraskuu", "joulukuu"};
-// f) Kirjoita luokkaan instanssimetodi, joka palauttaa päivämäärän merkkijonona pitkässä muodossa, esim.
-// 9. toukokuuta, 2017
-// g) Kirjoita luokkametodi, jolle välitetään parametrina kuukauden nimi merkkijonona, ja metodi palauttaa
-// kuukauden numeron. Tee metodista sellainen, että ei ole väliä, onko kuukauden nimi kirjoitettu isoilla vai
-// pienillä kirjaimilla.
-// Kirjoita main-metodiin lauseet, joilla kokeilet näiden metodien toimintaa.
-// (Huom. Java APIssa on valmiina useita luokkia päivämäärien käsittelyyn, kuten java.util.Date,
-// java.util.Calendar ja java.time.LocalDate. Kirjoitetaan tämä oma luokka oppimistarkoituksessa ja sen
-// kehittämistä ja käyttöä jatketaan myös seuraavissa harjoituksissa.)
-
 public class main {
     public static void main(String[] args) {
-        dates_class dates = new dates_class("20");
-        String text = dates.toString();
-        System.out.println(text);
-        // rectangle test;
-        // double low[] = {0,0};
-        // double high[] = {10,10};
-        // test = new rectangle(low, high);
-        // test.get_data();
-        // test = new rectangle();
-        // test.get_data();
+        dates_class dates1 = new dates_class();
+        dates_class dates2 = new dates_class();
+        boolean a = dates1.is_same(dates2);
+        // String text = dates.toString();
+        System.out.println(dates1.get_full_date());
+
     }
 
 }
 
 
-// a) Kirjoita luokka, joka kuvaa päivämäärän piirteet (tietoina kokonaisluvut päivä, kuukausi ja vuosi).
-// Määrittele tiedot yksityisiksi ja kirjoita niille setterit ja getterit (Vinkki: saat setterit ja getterit Eclipsen
-// avulla valitsemalla Source-valikosta Generate Getters and Setters…). Kirjoita luokkaan myös metodi
-// public String toString()
+
 public class dates_class {
     private int date;
     private int month;
     private int year;
+    public static final String[] MONHTS = {"jan", "feb",
+    "mar", "apr", "may", "jun", "jul", "aug",
+    "sep", "oct", "nov", "dec"};
 
     public dates_class() {
         java.time.LocalDate today = java.time.LocalDate.now();
@@ -84,12 +46,15 @@ public class dates_class {
         }
     }
 
-    // Kirjoita metodi public boolean onSama(Paivamaara toinen), joka palauttaa totuusarvon, onko
-    // tämän olion päivämäärä sama kuin parametrina välitetty toinen päivämäärä. (Käytä parametrille
-    // tyyppinä omaa kirjoittamaasi luokkatyyppiä Paivamaaran sijaan.)
-    public dates_class is_same(dates_class test_date) {
-        int i = 0;
-        return test_date;
+    public String get_full_date() {
+        return (this.date + "." + MONHTS[this.month-1] + "." + this.year);
+    }
+
+    public boolean is_same(dates_class test_date) {
+        if ((this.date != test_date.date) || (this.month != test_date.month) || (this.year != test_date.year)) {
+            return false;
+        }
+        return true;
     }
 
     public void set_date(int new_date) {
