a7e66da572b310090e707ec95c849ed53a0a9c5d
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-08-26 19:17:37 +0300

    Done hashmap test

--- hashmaptest.java
+++ hashmaptest.java
@@ -11,17 +11,51 @@ import java.util.HashMap;
 public class hashmaptest {
     public static void main(String[] args) {
         Scanner scanner_obj = new Scanner(System.in);
-        String user_input = "exmpl";
+        HashMap<Integer, Double> product_map = new HashMap<Integer, Double>();
+        Integer user_input_product_id = 1;
+        Double user_input_product_price = 5.0;
+        // people.put("John", 32)
 
-        while (user_input.equals("") != true) {
-            System.out.println("Give dates: (emptyline to exit)");
-            user_input = scanner_obj.nextLine();
-
-            if (user_input.equals("") != true) {
-
+        while (user_input_product_id != 0) {
+            System.out.println("Give product id: (0 to quit)");
+            user_input_product_id = scanner_obj.nextInt();
+            if (user_input_product_id != 0) {
+                System.out.println("Give produt price: ");
+                user_input_product_price = scanner_obj.nextDouble();
+                product_map.put(user_input_product_id, user_input_product_price);
             }
         }
+        user_input_product_id = 1;
+        while (user_input_product_id != 0) {
+            System.out.println("Give product id to find: (0 to quit)");
+            user_input_product_id = scanner_obj.nextInt();
+            if (product_map.get(user_input_product_id) != null) {
+                System.out.println("Found: " + product_map.get(user_input_product_id));
+            }
+            else {
+                System.out.println("Not found");
+            }
 
+        }
+        System.out.println(product_map);
         scanner_obj.close();
     }
 }
+
+// HashMap<String, String> capitalCities = new HashMap<String, String>();
+// capitalCities.put("England", "London");
+// capitalCities.get("England");
+// capitalCities.remove("England");
+// apitalCities.clear();
+// capitalCities.size();
+// Print keys
+
+// loop key
+// for (String i : capitalCities.keySet()) {
+//     System.out.println(i);
+//   }
+
+
+// HashMap<String, Integer> people = new HashMap<String, Integer>();
+// // Add keys and values (Name, Age)
+// people.put("John", 32)
